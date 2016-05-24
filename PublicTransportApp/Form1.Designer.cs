2d529460e8bf4d0b5d50afbaba0d473f24298fc5
Michelle <mi.meyer@hotmail.ch>
2016-05-24 16:17:12 +0200

    Created form; started with controls

---
+++ PublicTransportApp/Form1.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace PublicTransportApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabConnection = new System.Windows.Forms.TabPage();
+            this.tabStation = new System.Windows.Forms.TabPage();
+            this.tabStart = new System.Windows.Forms.TabPage();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.btnDeparture = new System.Windows.Forms.Button();
+            this.btnArrival = new System.Windows.Forms.Button();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.tabControl1.SuspendLayout();
+            this.tabConnection.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabStart);
+            this.tabControl1.Controls.Add(this.tabConnection);
+            this.tabControl1.Controls.Add(this.tabStation);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(693, 559);
+            this.tabControl1.TabIndex = 1;
+            // 
+            // tabConnection
+            // 
+            this.tabConnection.Controls.Add(this.splitContainer1);
+            this.tabConnection.Location = new System.Drawing.Point(4, 22);
+            this.tabConnection.Name = "tabConnection";
+            this.tabConnection.Padding = new System.Windows.Forms.Padding(3);
+            this.tabConnection.Size = new System.Drawing.Size(685, 533);
+            this.tabConnection.TabIndex = 0;
+            this.tabConnection.Text = "Search Connection";
+            this.tabConnection.UseVisualStyleBackColor = true;
+            // 
+            // tabStation
+            // 
+            this.tabStation.Location = new System.Drawing.Point(4, 22);
+            this.tabStation.Name = "tabStation";
+            this.tabStation.Padding = new System.Windows.Forms.Padding(3);
+            this.tabStation.Size = new System.Drawing.Size(650, 452);
+            this.tabStation.TabIndex = 1;
+            this.tabStation.Text = "Search Station";
+            this.tabStation.UseVisualStyleBackColor = true;
+            // 
+            // tabStart
+            // 
+            this.tabStart.Location = new System.Drawing.Point(4, 22);
+            this.tabStart.Name = "tabStart";
+            this.tabStart.Size = new System.Drawing.Size(685, 533);
+            this.tabStart.TabIndex = 2;
+            this.tabStart.Text = "Start";
+            this.tabStart.UseVisualStyleBackColor = true;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(3, 3);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.dateTimePicker2);
+            this.splitContainer1.Panel1.Controls.Add(this.btnArrival);
+            this.splitContainer1.Panel1.Controls.Add(this.btnDeparture);
+            this.splitContainer1.Panel1.Controls.Add(this.dateTimePicker1);
+            this.splitContainer1.Panel1.Controls.Add(this.label4);
+            this.splitContainer1.Panel1.Controls.Add(this.label3);
+            this.splitContainer1.Panel1.Controls.Add(this.label2);
+            this.splitContainer1.Panel1.Controls.Add(this.label1);
+            this.splitContainer1.Size = new System.Drawing.Size(679, 527);
+            this.splitContainer1.SplitterDistance = 226;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.LimeGreen;
+            this.label1.Location = new System.Drawing.Point(287, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(119, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Connection";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(20, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "From";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(20, 88);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(20, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "To";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(20, 115);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(30, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Date";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(85, 115);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 4;
+            // 
+            // btnDeparture
+            // 
+            this.btnDeparture.Location = new System.Drawing.Point(353, 116);
+            this.btnDeparture.Name = "btnDeparture";
+            this.btnDeparture.Size = new System.Drawing.Size(75, 23);
+            this.btnDeparture.TabIndex = 5;
+            this.btnDeparture.Text = "Departure";
+            this.btnDeparture.UseVisualStyleBackColor = true;
+            // 
+            // btnArrival
+            // 
+            this.btnArrival.Location = new System.Drawing.Point(434, 116);
+            this.btnArrival.Name = "btnArrival";
+            this.btnArrival.Size = new System.Drawing.Size(75, 23);
+            this.btnArrival.TabIndex = 6;
+            this.btnArrival.Text = "Arrival";
+            this.btnArrival.UseVisualStyleBackColor = true;
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.dateTimePicker2.Location = new System.Drawing.Point(528, 119);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(102, 20);
+            this.dateTimePicker2.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(693, 559);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Public Transport App";
+            this.tabControl1.ResumeLayout(false);
+            this.tabConnection.ResumeLayout(false);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabConnection;
+        private System.Windows.Forms.TabPage tabStation;
+        private System.Windows.Forms.TabPage tabStart;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.Button btnArrival;
+        private System.Windows.Forms.Button btnDeparture;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
