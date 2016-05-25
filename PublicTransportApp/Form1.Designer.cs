dc7082d3f98703db6553b5de08832d8414e0a3c6
Michelle <mi.meyer@hotmail.ch>
2016-05-25 11:37:51 +0200

    added listview and FillListView with connections

--- PublicTransportApp/Form1.Designer.cs
+++ PublicTransportApp/Form1.Designer.cs
@@ -29,22 +29,27 @@
         private void InitializeComponent()
         {
             this.tabControl1 = new System.Windows.Forms.TabControl();
-            this.tabConnection = new System.Windows.Forms.TabPage();
-            this.tabStation = new System.Windows.Forms.TabPage();
             this.tabStart = new System.Windows.Forms.TabPage();
+            this.tabConnection = new System.Windows.Forms.TabPage();
             this.splitContainer1 = new System.Windows.Forms.SplitContainer();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
-            this.btnDeparture = new System.Windows.Forms.Button();
+            this.dtpTime = new System.Windows.Forms.DateTimePicker();
             this.btnArrival = new System.Windows.Forms.Button();
-            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.btnDeparture = new System.Windows.Forms.Button();
+            this.dtpDate = new System.Windows.Forms.DateTimePicker();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tabStation = new System.Windows.Forms.TabPage();
+            this.txtFrom = new System.Windows.Forms.TextBox();
+            this.txtTo = new System.Windows.Forms.TextBox();
+            this.btnSearchConnection = new System.Windows.Forms.Button();
+            this.lsvConnections = new System.Windows.Forms.ListView();
             this.tabControl1.SuspendLayout();
             this.tabConnection.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
             this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
             this.splitContainer1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -60,6 +65,15 @@
             this.tabControl1.Size = new System.Drawing.Size(693, 559);
             this.tabControl1.TabIndex = 1;
             // 
+            // tabStart
+            // 
+            this.tabStart.Location = new System.Drawing.Point(4, 22);
+            this.tabStart.Name = "tabStart";
+            this.tabStart.Size = new System.Drawing.Size(685, 533);
+            this.tabStart.TabIndex = 2;
+            this.tabStart.Text = "Start";
+            this.tabStart.UseVisualStyleBackColor = true;
+            // 
             // tabConnection
             // 
             this.tabConnection.Controls.Add(this.splitContainer1);
@@ -71,25 +85,6 @@
             this.tabConnection.Text = "Search Connection";
             this.tabConnection.UseVisualStyleBackColor = true;
             // 
-            // tabStation
-            // 
-            this.tabStation.Location = new System.Drawing.Point(4, 22);
-            this.tabStation.Name = "tabStation";
-            this.tabStation.Padding = new System.Windows.Forms.Padding(3);
-            this.tabStation.Size = new System.Drawing.Size(650, 452);
-            this.tabStation.TabIndex = 1;
-            this.tabStation.Text = "Search Station";
-            this.tabStation.UseVisualStyleBackColor = true;
-            // 
-            // tabStart
-            // 
-            this.tabStart.Location = new System.Drawing.Point(4, 22);
-            this.tabStart.Name = "tabStart";
-            this.tabStart.Size = new System.Drawing.Size(685, 533);
-            this.tabStart.TabIndex = 2;
-            this.tabStart.Text = "Start";
-            this.tabStart.UseVisualStyleBackColor = true;
-            // 
             // splitContainer1
             // 
             this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
@@ -99,17 +94,85 @@
             // 
             // splitContainer1.Panel1
             // 
-            this.splitContainer1.Panel1.Controls.Add(this.dateTimePicker2);
+            this.splitContainer1.Panel1.Controls.Add(this.btnSearchConnection);
+            this.splitContainer1.Panel1.Controls.Add(this.txtTo);
+            this.splitContainer1.Panel1.Controls.Add(this.txtFrom);
+            this.splitContainer1.Panel1.Controls.Add(this.dtpTime);
             this.splitContainer1.Panel1.Controls.Add(this.btnArrival);
             this.splitContainer1.Panel1.Controls.Add(this.btnDeparture);
-            this.splitContainer1.Panel1.Controls.Add(this.dateTimePicker1);
+            this.splitContainer1.Panel1.Controls.Add(this.dtpDate);
             this.splitContainer1.Panel1.Controls.Add(this.label4);
             this.splitContainer1.Panel1.Controls.Add(this.label3);
             this.splitContainer1.Panel1.Controls.Add(this.label2);
             this.splitContainer1.Panel1.Controls.Add(this.label1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.lsvConnections);
             this.splitContainer1.Size = new System.Drawing.Size(679, 527);
             this.splitContainer1.SplitterDistance = 226;
             this.splitContainer1.TabIndex = 0;
+            // 
+            // dtpTime
+            // 
+            this.dtpTime.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.dtpTime.Location = new System.Drawing.Point(529, 150);
+            this.dtpTime.MinDate = new System.DateTime(2016, 1, 1, 0, 0, 0, 0);
+            this.dtpTime.Name = "dtpTime";
+            this.dtpTime.Size = new System.Drawing.Size(102, 20);
+            this.dtpTime.TabIndex = 7;
+            // 
+            // btnArrival
+            // 
+            this.btnArrival.Location = new System.Drawing.Point(435, 147);
+            this.btnArrival.Name = "btnArrival";
+            this.btnArrival.Size = new System.Drawing.Size(75, 23);
+            this.btnArrival.TabIndex = 6;
+            this.btnArrival.Text = "Arrival";
+            this.btnArrival.UseVisualStyleBackColor = true;
+            // 
+            // btnDeparture
+            // 
+            this.btnDeparture.Location = new System.Drawing.Point(354, 147);
+            this.btnDeparture.Name = "btnDeparture";
+            this.btnDeparture.Size = new System.Drawing.Size(75, 23);
+            this.btnDeparture.TabIndex = 5;
+            this.btnDeparture.Text = "Departure";
+            this.btnDeparture.UseVisualStyleBackColor = true;
+            // 
+            // dtpDate
+            // 
+            this.dtpDate.Location = new System.Drawing.Point(86, 146);
+            this.dtpDate.Name = "dtpDate";
+            this.dtpDate.Size = new System.Drawing.Size(200, 20);
+            this.dtpDate.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(21, 146);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(30, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Date";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(20, 104);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(20, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "To";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(20, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "From";
             // 
             // label1
             // 
@@ -122,65 +185,51 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "Connection";
             // 
-            // label2
+            // tabStation
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(20, 63);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(30, 13);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "From";
+            this.tabStation.Location = new System.Drawing.Point(4, 22);
+            this.tabStation.Name = "tabStation";
+            this.tabStation.Padding = new System.Windows.Forms.Padding(3);
+            this.tabStation.Size = new System.Drawing.Size(685, 533);
+            this.tabStation.TabIndex = 1;
+            this.tabStation.Text = "Search Station";
+            this.tabStation.UseVisualStyleBackColor = true;
             // 
-            // label3
+            // txtFrom
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(20, 88);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(20, 13);
-            this.label3.TabIndex = 2;
-            this.label3.Text = "To";
+            this.txtFrom.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtFrom.Location = new System.Drawing.Point(86, 60);
+            this.txtFrom.Name = "txtFrom";
+            this.txtFrom.Size = new System.Drawing.Size(200, 20);
+            this.txtFrom.TabIndex = 8;
             // 
-            // label4
+            // txtTo
             // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(20, 115);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(30, 13);
-            this.label4.TabIndex = 3;
-            this.label4.Text = "Date";
+            this.txtTo.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtTo.Location = new System.Drawing.Point(86, 101);
+            this.txtTo.Name = "txtTo";
+            this.txtTo.Size = new System.Drawing.Size(200, 20);
+            this.txtTo.TabIndex = 9;
             // 
-            // dateTimePicker1
+            // btnSearchConnection
             // 
-            this.dateTimePicker1.Location = new System.Drawing.Point(85, 115);
-            this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
-            this.dateTimePicker1.TabIndex = 4;
+            this.btnSearchConnection.Location = new System.Drawing.Point(475, 200);
+            this.btnSearchConnection.Name = "btnSearchConnection";
+            this.btnSearchConnection.Size = new System.Drawing.Size(156, 23);
+            this.btnSearchConnection.TabIndex = 10;
+            this.btnSearchConnection.Text = "Search Connection";
+            this.btnSearchConnection.UseVisualStyleBackColor = true;
+            this.btnSearchConnection.Click += new System.EventHandler(this.btnSearchConnection_Click);
             // 
-            // btnDeparture
+            // lsvConnections
             // 
-            this.btnDeparture.Location = new System.Drawing.Point(353, 116);
-            this.btnDeparture.Name = "btnDeparture";
-            this.btnDeparture.Size = new System.Drawing.Size(75, 23);
-            this.btnDeparture.TabIndex = 5;
-            this.btnDeparture.Text = "Departure";
-            this.btnDeparture.UseVisualStyleBackColor = true;
-            // 
-            // btnArrival
-            // 
-            this.btnArrival.Location = new System.Drawing.Point(434, 116);
-            this.btnArrival.Name = "btnArrival";
-            this.btnArrival.Size = new System.Drawing.Size(75, 23);
-            this.btnArrival.TabIndex = 6;
-            this.btnArrival.Text = "Arrival";
-            this.btnArrival.UseVisualStyleBackColor = true;
-            // 
-            // dateTimePicker2
-            // 
-            this.dateTimePicker2.Format = System.Windows.Forms.DateTimePickerFormat.Time;
-            this.dateTimePicker2.Location = new System.Drawing.Point(528, 119);
-            this.dateTimePicker2.Name = "dateTimePicker2";
-            this.dateTimePicker2.Size = new System.Drawing.Size(102, 20);
-            this.dateTimePicker2.TabIndex = 7;
+            this.lsvConnections.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lsvConnections.Location = new System.Drawing.Point(0, 0);
+            this.lsvConnections.Name = "lsvConnections";
+            this.lsvConnections.Size = new System.Drawing.Size(679, 297);
+            this.lsvConnections.TabIndex = 0;
+            this.lsvConnections.UseCompatibleStateImageBehavior = false;
+            this.lsvConnections.View = System.Windows.Forms.View.Details;
             // 
             // Form1
             // 
@@ -194,6 +243,7 @@
             this.tabConnection.ResumeLayout(false);
             this.splitContainer1.Panel1.ResumeLayout(false);
             this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
             this.splitContainer1.ResumeLayout(false);
             this.ResumeLayout(false);
@@ -207,13 +257,17 @@
         private System.Windows.Forms.TabPage tabStart;
         private System.Windows.Forms.SplitContainer splitContainer1;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.DateTimePicker dtpTime;
         private System.Windows.Forms.Button btnArrival;
         private System.Windows.Forms.Button btnDeparture;
-        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DateTimePicker dtpDate;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtTo;
+        private System.Windows.Forms.TextBox txtFrom;
+        private System.Windows.Forms.Button btnSearchConnection;
+        private System.Windows.Forms.ListView lsvConnections;
     }
 }
 
--- PublicTransportApp/Form1.cs
+++ PublicTransportApp/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using SwissTransport;
 
 namespace PublicTransportApp
 {
@@ -15,6 +16,46 @@ namespace PublicTransportApp
         public Form1()
         {
             InitializeComponent();
+            //
+            dtpTime.Format = DateTimePickerFormat.Custom;
+            dtpTime.CustomFormat = "HH:mm";
+            dtpTime.ShowUpDown = true;
+
+            //lsvConnections.Columns.Add("From");
+            //lsvConnections.Columns.Add("To");
+            lsvConnections.Columns.Add("Departure time");
+            lsvConnections.Columns.Add("Arrival time");
+            lsvConnections.Columns.Add("Duration");
+            lsvConnections.Columns.Add("Departure platform");
+            lsvConnections.Columns.Add("Arrival platform");
+        }
+
+        private void btnSearchConnection_Click(object sender, EventArgs e)
+        {
+            lsvConnections.Items.Clear();
+            var placeOfDeparture = txtFrom.Text ?? "";
+            var destination = txtTo.Text ?? "";
+            var date = dtpDate.Value;
+            var time = dtpTime.Value;
+
+            var transport = new Transport();
+            Connections connections = transport.GetConnections(placeOfDeparture, destination);
+
+            FillListView(connections);
+        }
+
+        private void FillListView(Connections connections)
+        {
+            foreach (var con in connections.ConnectionList)
+            {
+                var item = new ListViewItem(con.From.Departure);
+                item.SubItems.Add(con.To.Arrival);
+                item.SubItems.Add(con.Duration);
+                item.SubItems.Add(con.From.Platform);
+                item.SubItems.Add(con.To.Platform);
+
+                lsvConnections.Items.Add(item);
+            }
         }
     }
 }
